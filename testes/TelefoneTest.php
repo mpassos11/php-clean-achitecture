2b6870a29985e4ab1db1d295259e2c249c02fc5b
Matheus Passos <matheus.passos@multidadosti.com.br>
2022-11-28 21:11:16 -0300

    aprimorando lista de telefones com validcoes + testes unitarios

---
+++ testes/TelefoneTest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+use Alura\Arquitetura\Telefone;
+use PHPUnit\Framework\TestCase;
+
+class TelefoneTest extends TestCase
+{
+	public function testTelefoneDevePoderSerRepresentadoComoString()
+	{
+		$telefone = new Telefone('11', '960734956');
+		$this->assertSame('(11) 960734956', (string) $telefone);
+	}
+	
+	public function testTelefoneComDddInvalidoNaoDeveExistir()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+		$this->expectDeprecationMessage('DDD inválido');
+		new Telefone('ddd', '960734956');
+	}
+	
+	public function testTelefoneComNumeroInvalidoNaoDeveExistir()
+	{
+		$this->expectException(\InvalidArgumentException::class);
+		$this->expectDeprecationMessage('Número de telefone inválido');
+		new Telefone('11', 'numero123');
+	}
+}
